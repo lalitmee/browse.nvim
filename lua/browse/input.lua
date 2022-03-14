61529661f65ab59ff2703cfc186842d73b0bf3ec
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-03-14 11:24:12 +0530

    fix: 🐛 input search on_confirm function

--- lua/browse/input.lua
+++ lua/browse/input.lua
@@ -3,12 +3,13 @@ local utils = require("browse.utils")
 local M = {}
 
 M.search_input = function()
-  local input = vim.ui.input("Search String: ")
-  if input == nil or input == "" then
-    return
-  end
-  local open_cmd = utils.get_open_cmd()
-  vim.fn.jobstart(string.format("%s 'https://www.google.com/search?q=%s'", open_cmd, input))
+  vim.ui.input("Search String: ", function(input)
+    if input == nil or input == "" then
+      return
+    end
+    local open_cmd = utils.get_open_cmd()
+    vim.fn.jobstart(string.format("%s 'https://www.google.com/search?q=%s'", open_cmd, input))
+  end)
 end
 
 return M
