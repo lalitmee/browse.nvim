d5366f17b1ff7a4449c5f9b104a9ab6ca5f8976c
green726 <59667714+green726@users.noreply.github.com>
2022-03-19 16:48:27 -0400

    went back to jobstart - and formatted

--- lua/browse/input.lua
+++ lua/browse/input.lua
@@ -3,15 +3,14 @@ local utils = require("browse.utils")
 local M = {}
 
 M.search_input = function()
-  vim.ui.input("Search String: ", function(input)
-    if input == nil or input == "" then
-      return
-    end
-    local open_cmd = utils.get_open_cmd()
-    local cmd = open_cmd.." https://www.google.com/search?q="..input
-    vim.cmd(":silent ! "..cmd)
-    -- vim.fn.jobstart(string.format("%s 'https://www.google.com/search?q=%s'", open_cmd, input))
-  end)
+	vim.ui.input("Search String: ", function(input)
+		if input == nil or input == "" then
+			return
+		end
+		local open_cmd = utils.get_open_cmd()
+		local cmd = open_cmd .. " https://www.google.com/search?q=" .. input
+		vim.fn.jobstart(cmd)
+	end)
 end
 
 return M
