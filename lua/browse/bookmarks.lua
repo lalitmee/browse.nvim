553969648fe25d5759f0d63e8e11dc2656b2ef13
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-04-12 22:58:46 +0530

    fix(formatting): format using spaces

--- lua/browse/bookmarks.lua
+++ lua/browse/bookmarks.lua
@@ -11,26 +11,26 @@ local M = {}
 
 -- search bookmarks
 M.search_bookmarks = function(config)
-	local bookmarks = config["bookmarks"] or {}
-	local theme = themes.get_dropdown()
-	local opts = vim.tbl_deep_extend("force", config, theme or {})
-	pickers.new(opts, {
-		prompt_title = "Bookmarks",
-		finder = finders.new_table({
-			results = bookmarks,
-		}),
-		sorter = conf.generic_sorter(opts),
-		attach_mappings = function(prompt_bufnr, _)
-			actions.select_default:replace(function()
-				actions.close(prompt_bufnr)
-				local selection = action_state.get_selected_entry()
-				local open_cmd = utils.get_open_cmd()
-				local cmd = open_cmd .. " " .. selection[1]
-				vim.fn.jobstart(cmd)
-			end)
-			return true
-		end,
-	}):find()
+  local bookmarks = config["bookmarks"] or {}
+  local theme = themes.get_dropdown()
+  local opts = vim.tbl_deep_extend("force", config, theme or {})
+  pickers.new(opts, {
+    prompt_title = "Bookmarks",
+    finder = finders.new_table({
+      results = bookmarks,
+    }),
+    sorter = conf.generic_sorter(opts),
+    attach_mappings = function(prompt_bufnr, _)
+      actions.select_default:replace(function()
+        actions.close(prompt_bufnr)
+        local selection = action_state.get_selected_entry()
+        local open_cmd = utils.get_open_cmd()
+        local cmd = open_cmd .. " " .. selection[1]
+        vim.fn.jobstart(cmd)
+      end)
+      return true
+    end,
+  }):find()
 end
 
 return M
--- lua/browse/input.lua
+++ lua/browse/input.lua
@@ -3,14 +3,14 @@ local utils = require("browse.utils")
 local M = {}
 
 M.search_input = function()
-	vim.ui.input("Search String: ", function(input)
-		if input == nil or input == "" then
-			return
-		end
-		local open_cmd = utils.get_open_cmd()
-		local cmd = open_cmd .. " https://www.google.com/search?q=" .. input
-		vim.fn.jobstart(cmd)
-	end)
+  vim.ui.input("Search String: ", function(input)
+    if input == nil or input == "" then
+      return
+    end
+    local open_cmd = utils.get_open_cmd()
+    local cmd = open_cmd .. " https://www.google.com/search?q=" .. input
+    vim.fn.jobstart(cmd)
+  end)
 end
 
 return M
