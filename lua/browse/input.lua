977c0ce69e3020ac4d317c0fd2fd27584566c567
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-06-27 15:51:50 +0530

    fix(jobstart): fix `vim.fn.jobstart()` use

--- lua/browse/input.lua
+++ lua/browse/input.lua
@@ -8,8 +8,8 @@ M.search_input = function()
       return
     end
     local open_cmd = utils.get_open_cmd()
-    local cmd = open_cmd .. " https://www.google.com/search?q=" .. input
-    vim.fn.jobstart(cmd)
+    local path = "https://www.google.com/search?q=" .. input
+    vim.fn.jobstart({ open_cmd, path }, { detach = true })
   end)
 end
 
