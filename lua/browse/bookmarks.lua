e8427ab110304dd788b61100f91c28b23dbea614
B4mbus <peaceofloneliness@gmail.com>
2022-08-06 17:50:26 +0200

    feat: use browser module inside bookmarks

--- lua/browse/bookmarks.lua
+++ lua/browse/bookmarks.lua
@@ -5,7 +5,7 @@ local actions = require("telescope.actions")
 local themes = require("telescope.themes")
 local action_state = require("telescope.actions.state")
 
-local utils = require("browse.utils")
+local browser = require("browse.browser")
 
 local M = {}
 
@@ -14,19 +14,25 @@ M.search_bookmarks = function(config)
   local bookmarks = config["bookmarks"] or {}
   local theme = themes.get_dropdown()
   local opts = vim.tbl_deep_extend("force", config, theme or {})
+
   pickers.new(opts, {
     prompt_title = "Bookmarks",
     finder = finders.new_table({
       results = bookmarks,
     }),
+
     sorter = conf.generic_sorter(opts),
+
     attach_mappings = function(prompt_bufnr, _)
       actions.select_default:replace(function()
         actions.close(prompt_bufnr)
         local selection = action_state.get_selected_entry()
-        local open_cmd = utils.get_open_cmd()
-        local cmd = open_cmd .. " " .. selection[1]
-        vim.fn.jobstart(cmd)
+
+				if not selection then
+					return
+				end
+
+				browser.plain_search(selection[1])
       end)
       return true
     end,
