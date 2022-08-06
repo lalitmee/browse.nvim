1b57d4037934b1974c10e78ca33805613c8832e6
B4mbus <peaceofloneliness@gmail.com>
2022-08-06 17:51:09 +0200

    feat: use browser module inside input

--- lua/browse/input.lua
+++ lua/browse/input.lua
@@ -1,27 +1,23 @@
-local utils = require("browse.utils")
+local browser = require("browse.browser")
 local config = require("browse.config")
 
 local fmt = string.format
 
 local M = {}
 
-M.search_input = function()
-  vim.ui.input("Search String: ", function(input)
-    if input == nil or input == "" then
-      return
-    end
-    local open_cmd = utils.get_open_cmd()
-    local provider = config.options.provider
-    local path = fmt("https://%s.com/search?q=%s", provider, input)
-    if provider == "google" or provider == "bing" then
-      path = fmt("https://%s.com/search?q=%s", provider, input)
-    elseif provider == "brave" then
-      path = fmt("https://search.%s.com/search?q=%s", provider, input)
-    elseif provider == "duckduckgo" then
-      path = fmt("https://%s.com/?q=%s", provider, input)
-    end
-    vim.fn.jobstart({ open_cmd, path }, { detach = true })
-  end)
-end
+M.search_input = browser.generic_search_custom(function(input)
+	local provider = config.options.provider
+	local path = fmt("https://%s.com/search?q=%s", provider, input)
+
+	if provider == "google" or provider == "bing" then
+		path = fmt("https://%s.com/search?q=%s", provider, input)
+	elseif provider == "brave" then
+		path = fmt("https://search.%s.com/search?q=%s", provider, input)
+	elseif provider == "duckduckgo" then
+		path = fmt("https://%s.com/?q=%s", provider, input)
+	end
+
+	return path
+end)
 
 return M
