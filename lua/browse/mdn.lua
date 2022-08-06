d26e5ea20a53c1814c0cc73b0c1c59c2dfc84923
B4mbus <peaceofloneliness@gmail.com>
2022-08-06 17:51:18 +0200

    feat: use browser module inside mdn

--- lua/browse/mdn.lua
+++ lua/browse/mdn.lua
@@ -1,15 +1,7 @@
-local utils = require("browse.utils")
+local browser = require("browse.browser")
 
 local M = {}
 
-M.search = function()
-  vim.ui.input("Search String: ", function(input)
-    if input == nil or input == "" then
-      return
-    end
-    local open_cmd = utils.get_open_cmd()
-    vim.fn.jobstart(string.format("%s 'https://developer.mozilla.org/en-US/search?q=%s'", open_cmd, input))
-  end)
-end
+M.search = browser.generic_search_for("https://developer.mozilla.org/en-US/search?q=%s")
 
 return M
