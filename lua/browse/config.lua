ae79ad1828690cb38f26aff9498f636a53cfff5b
B4mbus <peaceofloneliness@gmail.com>
2022-08-07 10:07:52 +0200

    style: use 2 width spaces instead of tabs

--- lua/browse/config.lua
+++ lua/browse/config.lua
@@ -7,7 +7,7 @@ local defaults = {
 M.options = defaults
 
 function M.setup(opts)
-  M.options = vim.tbl_extend('keep', defaults, opts or {})
+  M.options = vim.tbl_extend("keep", defaults, opts or {})
 end
 
 return M
--- lua/browse/devdocs.lua
+++ lua/browse/devdocs.lua
@@ -3,9 +3,9 @@ local browser = require("browse.browser")
 local M = {}
 
 M.search_with_filetype = browser.generic_search_custom(function(input)
-	local input_with_filetype = vim.bo.filetype .. " " .. input
+  local input_with_filetype = vim.bo.filetype .. " " .. input
 
-	return string.format("https://devdocs.io/#q=%s", input_with_filetype)
+  return string.format("https://devdocs.io/#q=%s", input_with_filetype)
 end)
 
 M.search = browser.generic_search_for("https://devdocs.io/#q=%s")
