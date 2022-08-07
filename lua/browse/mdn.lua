5011a6846c706c3324df944513f94bec4c408c0e
B4mbus <peaceofloneliness@gmail.com>
2022-08-07 10:20:50 +0200

    refactor: change searching functions' names

--- lua/browse/mdn.lua
+++ lua/browse/mdn.lua
@@ -2,6 +2,6 @@ local utils = require("browse.utils")
 
 local M = {}
 
-M.search = utils.generic_search_for("https://developer.mozilla.org/en-US/search?q=%s")
+M.search = utils.format_search("https://developer.mozilla.org/en-US/search?q=%s")
 
 return M
