0167302538de54a162e5bc6951dc598dda956675
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2022-10-26 10:50:41 +0530

    fix(config): fix config overwrite

--- lua/browse/config.lua
+++ lua/browse/config.lua
@@ -1,13 +1,13 @@
 local M = {}
 
 local defaults = {
-  provider = "google",
+    provider = "google",
 }
 
 M.options = defaults
 
 function M.setup(opts)
-  M.options = vim.tbl_extend("keep", defaults, opts or {})
+    M.options = vim.tbl_extend("keep", opts, opts or defaults)
 end
 
 return M
