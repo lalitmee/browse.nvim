7a377904d5d6c394b7fcc9546216e5fdb4c9da4f
Quminouse <119440954+Quminouse@users.noreply.github.com>
2022-12-10 10:15:05 -0500

    Create example_bookmarks.lua

---
+++ example_bookmarks.lua
@@ -0,0 +1,11 @@
+local bookmarks = {
+  ["neovim"] = "https://neovim.discourse.group/",
+  ["github"] = {
+    ["neovim"] = "https://github.com/neovim/neovim",
+    "https://github.com/rockerBOO/awesome-neovim",
+    [".nvim"] = {
+      ["lualine"] = "https://github.com/hoob3rt/lualine.nvim",
+      ["telescope"] = "https://github.com/nvim-telescope/telescope.nvim",
+    },
+  },
+}
