edbd6819d923fddefdcfe324e515a8c3eaf11a70
Quminouse <119440954+Quminouse@users.noreply.github.com>
2022-12-10 10:15:54 -0500

    Update example_bookmarks.lua

--- example_bookmarks.lua
+++ example_bookmarks.lua
@@ -1,8 +1,8 @@
 local bookmarks = {
   ["neovim"] = "https://neovim.discourse.group/",
   ["github"] = {
-    ["neovim"] = "https://github.com/neovim/neovim",
     "https://github.com/rockerBOO/awesome-neovim",
+    ["neovim"] = "https://github.com/neovim/neovim",
     [".nvim"] = {
       ["lualine"] = "https://github.com/hoob3rt/lualine.nvim",
       ["telescope"] = "https://github.com/nvim-telescope/telescope.nvim",
