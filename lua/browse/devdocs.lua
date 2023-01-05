7f40b138231440cb46c157ed1a1e65e824f1383e
Mischa Molhoek <mischamolhoek@gmail.com>
2023-01-05 11:10:04 +0100

    fix search with filetype

--- lua/browse/devdocs.lua
+++ lua/browse/devdocs.lua
@@ -3,7 +3,7 @@ local utils = require("browse.utils")
 local M = {}
 
 M.search_with_filetype = utils.callback_search(function(input)
-  local input_with_filetype = vim.bo.filetype .. " " .. input
+  local input_with_filetype = vim.bo.filetype .. "%20" .. input
 
   return string.format("https://devdocs.io/#q=%s", input_with_filetype)
 end)
