3336967066f57af56e6d2a8c07ac09acce351cd5
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2023-02-02 21:33:22 +0530

    feat(bookmarks): support grouped bookmarks

--- lua/browse/bookmarks.lua
+++ lua/browse/bookmarks.lua
@@ -17,59 +17,84 @@ M.search_bookmarks = function(config)
     local theme = themes.get_dropdown()
     local opts = vim.tbl_deep_extend("force", config, theme or {})
 
-    local search = {}
+    local bookmarks_list = {}
 
     for k, v in pairs(bookmarks) do
         if type(k) == "string" then
-            table.insert(search, { k, v })
+            table.insert(bookmarks_list, { k, v })
         else
-            table.insert(search, v)
+            table.insert(bookmarks_list, v)
         end
+    end
+
+    local function create_finder()
+        return finders.new_table({
+            results = bookmarks_list,
+            entry_maker = function(entry)
+                -- when entry is a direct url
+                if type(entry) == "string" then
+                    return {
+                        value = entry,
+                        display = entry,
+                        ordinal = entry,
+                    }
+                -- when entry is an alias for either a url or a format url
+                elseif type(entry) == "table" and type(entry[2]) ~= "table" then
+                    return {
+                        value = entry[2],
+                        display = entry[1] .. " -> " .. entry[2],
+                        ordinal = entry[1] .. " " .. entry[2],
+                    }
+                -- when entry is a group of bookmarks
+                elseif type(entry) == "table" and type(entry[2]) == "table" then
+                    return {
+                        value = entry[2],
+                        display = entry[1]
+                            .. " -> "
+                            .. (entry[2]["name"] or ""),
+                        ordinal = entry[1] .. " " .. (entry[2]["name"] or ""),
+                    }
+                end
+            end,
+        })
     end
 
     pickers
         .new(opts, {
             prompt_title = "Bookmarks",
-            finder = finders.new_table({
-                results = search,
-                entry_maker = function(entry)
-                    -- when entry is a direct url
-                    if type(entry) == "string" then
-                        return {
-                            value = entry,
-                            display = entry,
-                            ordinal = entry,
-                        }
-                    end
-
-                    -- when entry is an alias for either a url or a format url
-                    if type(entry[2]) == "string" then
-                        return {
-                            value = entry,
-                            display = entry[1] .. " -> " .. entry[2],
-                            ordinal = entry[2],
-                        }
-                    end
-                end,
-            }),
-
+            finder = create_finder(),
             sorter = conf.generic_sorter(opts),
-
             attach_mappings = function(prompt_bufnr, _)
                 actions.select_default:replace(function()
                     actions.close(prompt_bufnr)
                     local selection = action_state.get_selected_entry()
+                    local value = selection["value"]
 
                     if not selection then
                         return
                     end
 
-                    local url = selection["ordinal"]
+                    local function remove_element(tbl, key)
+                        local element = tbl[key]
+                        local new_tbl = {}
+                        for k, v in pairs(tbl) do
+                            if k ~= key then
+                                new_tbl[k] = v
+                            end
+                        end
+                        return element, new_tbl
+                    end
 
-                    if string.find(url, "=") then
-                        utils.format_search(url)()
+                    if type(value) == "table" then
+                        local _, list = remove_element(value, "name")
+                        M.search_bookmarks({ bookmarks = list })
                     else
-                        utils.default_search(url)
+                        if string.find(value, "=") then
+                            utils.format_search(value)()
+                        else
+                            utils.default_search(value)
+                            vim.notify(string.format("Opening '%s'", value))
+                        end
                     end
                 end)
                 return true
