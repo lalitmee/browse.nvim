e946a34a418cff5617ca232be1b0343d9cf111e5
Lalit Kumar <lalit@quartic.ai>
2023-08-14 14:35:26 +0530

    refactor(bookmarks): copy bookmarks before mutation

--- lua/browse/bookmarks.lua
+++ lua/browse/bookmarks.lua
@@ -14,12 +14,13 @@ local M = {}
 M.search_bookmarks = function(config)
     config = config or {}
     local bookmarks = config["bookmarks"] or defaults.opts["bookmarks"] or {}
+    local bookmarks_copy = vim.deepcopy(bookmarks)
     local theme = themes.get_dropdown()
     local opts = vim.tbl_deep_extend("force", config, theme or {})
 
     local bookmarks_list = {}
 
-    for k, v in pairs(bookmarks) do
+    for k, v in pairs(bookmarks_copy) do
         if type(k) == "string" then
             table.insert(bookmarks_list, { k, v })
         else
@@ -27,36 +28,45 @@ M.search_bookmarks = function(config)
         end
     end
 
+    local function entry_maker(entry)
+        local value, display, ordinal
+
+        if type(entry) == "string" then
+            value = entry
+            display = entry
+            ordinal = entry
+        elseif type(entry) == "table" and type(entry[2]) ~= "table" then
+            value = entry[2]
+            display = entry[1] .. " -> " .. value
+            ordinal = value
+        elseif type(entry) == "table" and type(entry[2]) == "table" then
+            value = entry[2]
+            display = entry[1] .. " -> " .. (entry[2]["name"] or "")
+            ordinal = (entry[2]["name"] or "")
+        end
+
+        return {
+            value = value,
+            display = display,
+            ordinal = ordinal,
+        }
+    end
+
     local function create_finder()
         return finders.new_table({
             results = bookmarks_list,
-            entry_maker = function(entry)
-                -- when entry is a direct url
-                if type(entry) == "string" then
-                    return {
-                        value = entry,
-                        display = entry,
-                        ordinal = entry,
-                    }
-                -- when entry is an alias for either a url or a format url
-                elseif type(entry) == "table" and type(entry[2]) ~= "table" then
-                    return {
-                        value = entry[2],
-                        display = entry[1] .. " -> " .. entry[2],
-                        ordinal = entry[1] .. " " .. entry[2],
-                    }
-                -- when entry is a group of bookmarks
-                elseif type(entry) == "table" and type(entry[2]) == "table" then
-                    return {
-                        value = entry[2],
-                        display = entry[1]
-                            .. " -> "
-                            .. (entry[2]["name"] or ""),
-                        ordinal = entry[1] .. " " .. (entry[2]["name"] or ""),
-                    }
-                end
-            end,
+            entry_maker = entry_maker,
         })
+    end
+
+    local function remove_element(tbl, key)
+        local new_tbl = {}
+        for k, v in pairs(tbl) do
+            if k ~= key then
+                new_tbl[k] = v
+            end
+        end
+        return new_tbl
     end
 
     pickers
@@ -67,37 +77,40 @@ M.search_bookmarks = function(config)
             attach_mappings = function(prompt_bufnr, _)
                 actions.select_default:replace(function()
                     actions.close(prompt_bufnr)
+
                     local selection = action_state.get_selected_entry()
-                    local value = selection["value"]
 
                     if not selection then
                         return
                     end
 
-                    local function remove_element(tbl, key)
-                        local element = tbl[key]
-                        local new_tbl = {}
-                        for k, v in pairs(tbl) do
-                            if k ~= key then
-                                new_tbl[k] = v
-                            end
-                        end
-                        return element, new_tbl
-                    end
+                    local value = selection["value"]
 
                     if type(value) == "table" then
-                        local _, list = remove_element(value, "name")
+                        -- copy table to avoid mutation
+                        local tbl_copy = vim.deepcopy(value)
+
+                        local list = remove_element(tbl_copy, "name")
+
+                        -- search bookmarks with the new list
                         M.search_bookmarks({ bookmarks = list })
-                    else
+                    elseif type(value) == "string" then
                         -- checking for `%` in the url
                         if string.match(value, "%%") then
-                            utils.format_search(value)()
+                            utils.format_search(
+                                value,
+                                { prompt = "Enter query" }
+                            )()
                         else
                             utils.default_search(value)
                             vim.notify(string.format("Opening '%s'", value))
                         end
+                    else
+                        -- handle other types
+                        print("else", value)
                     end
                 end)
+
                 return true
             end,
         })
